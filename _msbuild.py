542158da177c75e81d5d37a727e4228466949ec2
Steve Dower <steve.dower@microsoft.com>
2023-10-02 11:53:15 +0100

    Enable 3.12 support

--- _msbuild.py
+++ _msbuild.py
@@ -34,6 +34,7 @@ METADATA = {
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     "Requires-Python": ">=3.7",
 }
